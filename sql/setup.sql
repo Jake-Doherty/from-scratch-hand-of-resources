40fcbebc0d9d914706b95dc8d4992bd70ea95b00
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-14 00:30:14 -0800

    add data for next table

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,9 @@
 drop table
   if exists users;
 
+drop table
+  if exists plants;
+
 create table
   users (
     id bigint generated always as identity primary key,
@@ -22,4 +25,26 @@ values
   ('Reeba', 'MacBrearty'),
   ('Dael', 'Denty'),
   ('Ronica', 'Widdows'),
-  ('Tabor', 'Smithend')
+  ('Tabor', 'Smithend');
+
+create table
+  plants (
+    id bigint generated always as identity primary key,
+    common_name varchar,
+    plant_family varchar,
+    plant_scientific_name varchar
+  );
+
+insert into
+  plants (common_name, plant_family, plant_scientific_name)
+values
+  ('Mustang Monardella', 'Lamiaceae', 'Monardella lanceolata A. Gray var. microcephala A. Gray'),
+  ('Silky Lupine', 'Fabaceae', 'Lupinus sericeus Pursh ssp. sericeus'),
+  ('Mt. Hamilton Mock Stonecrop', 'Crassulaceae', 'Sedella pentandra H. Sharsm.'),
+  ('Puerto Rico Stopper', 'Myrtaceae', 'Eugenia bellonis Krug & Urb.'),
+  ('Catch Me If You Can', 'Euphorbiaceae', 'Acalypha amentacea Roxb.'),
+  ('Marsh Sandwort', 'Caryophyllaceae', 'Arenaria paludicola B.L. Rob.'),
+  ('Pohlia Moss', 'Bryaceae', 'Pohlia melanodon (Brid.) Shaw'),
+  ('Syngonanthus', 'Eriocaulaceae', 'Syngonanthus Ruhl.'),
+  ('Pinkflower Hedgehog Cactus', 'Cactaceae', 'Echinocereus fendleri (Engelm.) Sencke ex J.N. Haage ssp. fendleri'),
+  ('Moco De Pavo', 'Poaceae', 'Eriochrysis cayennensis P. Beauv.')

3d36605a2e872df4bf79f4bd5834200e6d0ef1d9
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-14 00:49:17 -0800

    add test for getAll plants route

--- __tests__/plants.test.js
+++ __tests__/plants.test.js
@@ -7,6 +7,74 @@ describe('routes for plants', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('GET /plants should get a list of all plants', async () => {
+    const resp = await request(app).get('/plants');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "common_name": "Mustang Monardella",
+          "id": "1",
+          "plant_family": "Lamiaceae",
+          "plant_scientific_name": "Monardella lanceolata A. Gray var. microcephala A. Gray",
+        },
+        Object {
+          "common_name": "Silky Lupine",
+          "id": "2",
+          "plant_family": "Fabaceae",
+          "plant_scientific_name": "Lupinus sericeus Pursh ssp. sericeus",
+        },
+        Object {
+          "common_name": "Mt. Hamilton Mock Stonecrop",
+          "id": "3",
+          "plant_family": "Crassulaceae",
+          "plant_scientific_name": "Sedella pentandra H. Sharsm.",
+        },
+        Object {
+          "common_name": "Puerto Rico Stopper",
+          "id": "4",
+          "plant_family": "Myrtaceae",
+          "plant_scientific_name": "Eugenia bellonis Krug & Urb.",
+        },
+        Object {
+          "common_name": "Catch Me If You Can",
+          "id": "5",
+          "plant_family": "Euphorbiaceae",
+          "plant_scientific_name": "Acalypha amentacea Roxb.",
+        },
+        Object {
+          "common_name": "Marsh Sandwort",
+          "id": "6",
+          "plant_family": "Caryophyllaceae",
+          "plant_scientific_name": "Arenaria paludicola B.L. Rob.",
+        },
+        Object {
+          "common_name": "Pohlia Moss",
+          "id": "7",
+          "plant_family": "Bryaceae",
+          "plant_scientific_name": "Pohlia melanodon (Brid.) Shaw",
+        },
+        Object {
+          "common_name": "Syngonanthus",
+          "id": "8",
+          "plant_family": "Eriocaulaceae",
+          "plant_scientific_name": "Syngonanthus Ruhl.",
+        },
+        Object {
+          "common_name": "Pinkflower Hedgehog Cactus",
+          "id": "9",
+          "plant_family": "Cactaceae",
+          "plant_scientific_name": "Echinocereus fendleri (Engelm.) Sencke ex J.N. Haage ssp. fendleri",
+        },
+        Object {
+          "common_name": "Moco De Pavo",
+          "id": "10",
+          "plant_family": "Poaceae",
+          "plant_scientific_name": "Eriochrysis cayennensis P. Beauv.",
+        },
+      ]
+    `);
+  });
   afterAll(() => {
     pool.end();
   });

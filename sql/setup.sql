cf2b82f73158612636b1b25bc95645a25bf4de33
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-11-13 20:40:57 -0800

    set up first table and workspace add data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+drop table
+  if exists users;
+
+create table
+  users (
+    id bigint generated always as identity primary key,
+    first_name varchar,
+    last_name varchar
+  );
+
+insert into
+  users (first_name, last_name)
+values
+  ('Chevy', 'Silversmidt'),
+  ('Estelle', 'Mitie'),
+  ('Gwenni', 'Marien'),
+  ('Wynny', 'Weymont'),
+  ('Raimund', 'Balshen'),
+  ('Erena', 'Eyeington'),
+  ('Reeba', 'MacBrearty'),
+  ('Dael', 'Denty'),
+  ('Ronica', 'Widdows'),
+  ('Tabor', 'Smithend')
